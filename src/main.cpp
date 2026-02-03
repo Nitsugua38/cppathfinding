8f3781e4f1b2d9a133639d34092bfc3d9dff1a2c
AugustinS <58813858+Nitsugua38@users.noreply.github.com>
2026-02-03 16:52:35 +0100

    feat: added BFS algo

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <chrono>
+#include <queue>
 
 using namespace std;
 
@@ -21,11 +22,18 @@ class MAP {
         void initMap(string mapPath);
 };
 
+
 class ALGOS {
+    private:
+        int moveX[4] = {1, -1, 0, 0};
+        int moveY[4] = {0, 0, 1, -1};
+
     public:
         void findStart(MAP& m);
         bool dfs(MAP& m, int CurrentX, int CurrentY);
+        void bfs(MAP& m, int CurrentX, int CurrentY);
 };
+
 
 class BENCHMARK {
     private:
@@ -103,26 +111,26 @@ void ALGOS::findStart(MAP& m) {
 
 bool ALGOS::dfs(MAP& m, int CurrentX, int CurrentY) {
 
-    // Step 1 : if blocked/visited -> abandon
+    // Step 1: if blocked/visited -> abandon
 
     if (m.mapMatrix[CurrentY][CurrentX] == '#' || m.mapMatrix[CurrentY][CurrentX] == '*' || m.mapMatrix[CurrentY][CurrentX] == 'v' || m.mapMatrix[CurrentY][CurrentX] == '.') {
         return false;
     };
 
 
-    // Step 2 : if arrived -> finish
+    // Step 2: if arrived -> finish
 
     if (m.mapMatrix[CurrentY][CurrentX] == 'E') {
         return true;
     };
 
 
-    // Step 3 : mark as visited
+    // Step 3: mark as visited
 
     if (m.mapMatrix[CurrentY][CurrentX] != 'S') m.mapMatrix[CurrentY][CurrentX] = 'v';
 
 
-    // Step 4 : explore neighbors
+    // Step 4: explore neighbors
 
     if (dfs(m, CurrentX + 1, CurrentY) || dfs(m, CurrentX - 1, CurrentY) || dfs(m, CurrentX, CurrentY + 1) || dfs(m, CurrentX, CurrentY - 1)) {
         if (m.mapMatrix[CurrentY][CurrentX] != 'S') m.mapMatrix[CurrentY][CurrentX] = '.';
@@ -130,6 +138,92 @@ bool ALGOS::dfs(MAP& m, int CurrentX, int CurrentY) {
     }
 
     return false;
+
+};
+
+
+
+
+
+
+
+
+// ---------------- BREADTH-FIRST SEARCH -------------------------
+
+void ALGOS::bfs(MAP& m, int CurrentX, int CurrentY) {
+
+
+    // Step 1: Create a queue and init parent
+
+    queue<pair<int, int>> processQueue;
+    processQueue.push({CurrentX, CurrentY});
+
+    bool endingFound = false;
+    int parentX[m.NUM_ROWS][m.NUM_COLUMNS];
+    int parentY[m.NUM_ROWS][m.NUM_COLUMNS];
+
+    parentX[CurrentY][CurrentX] = CurrentX;
+    parentY[CurrentY][CurrentX] = CurrentY;
+
+
+
+    // Step 2: Process the queue
+
+    while (!processQueue.empty() && !endingFound) {
+        
+        CurrentX = processQueue.front().first;
+        CurrentY = processQueue.front().second;
+        processQueue.pop();
+
+
+        // For each neighbour
+
+        for (int i = 0; i < 4; i++) {
+            int newX = CurrentX + moveX[i];
+            int newY = CurrentY + moveY[i];
+
+
+            if (m.mapMatrix[newY][newX] != '#' && m.mapMatrix[newY][newX] != '*' && m.mapMatrix[newY][newX] != 'v') {
+
+                parentX[newY][newX] = CurrentX;
+                parentY[newY][newX] = CurrentY;
+
+                
+
+                // Ending found: Stop and rebuild final path from parents
+                
+                if (m.mapMatrix[newY][newX] == 'E') {
+                    endingFound = true;
+
+                    int recX = newX;
+                    int recY = newY;
+
+                    while (recX != m.StartX || recY != m.StartY) {
+                        if (m.mapMatrix[recY][recX] != 'E') {
+                            m.mapMatrix[recY][recX] = '.';
+                        };
+                        int px = parentX[recY][recX];
+                        int py = parentY[recY][recX];
+                        recX = px;
+                        recY = py;
+                    };
+
+
+                    return;
+                }
+
+                
+
+                // Else, mark as visited and add neighbor to queue
+                
+                if (m.mapMatrix[newY][newX] != 'S' && m.mapMatrix[newY][newX] != 'E') {
+                    m.mapMatrix[newY][newX] = 'v';
+                    processQueue.push({newX, newY});
+                };
+                
+            };
+        };
+    };
 
 };
 
@@ -198,10 +292,14 @@ int main() {
 
     
     algos.findStart(workingMap);
-    cout << "Starting point is located at " << workingMap.StartX << "," << workingMap.StartY << endl << endl << "Result:" << endl << endl;
+    cout << "Starting point is located at " << workingMap.StartX << "," << workingMap.StartY << endl << endl << "DFS Result:" << endl << endl;
 
 
 
+
+
+
+    // Execute DFS
 
     MAP DFSworkingMap = workingMap;
     BENCHMARK DFSbenchmark;
@@ -216,7 +314,25 @@ int main() {
 
 
 
-    cout << endl << "Benchmark:" << endl << "DFS Path length is " << DFSbenchmark.pathLength(DFSworkingMap) << " and took " << DFStime << " ms to run." << endl;
+
+
+    // Execute BFS
+
+    MAP BFSworkingMap = workingMap;
+    BENCHMARK BFSbenchmark;
+
+    BFSbenchmark.startTimer();
+    algos.bfs(BFSworkingMap, BFSworkingMap.StartX, BFSworkingMap.StartY);
+    float BFStime = BFSbenchmark.stopTimer();
+
+    cout << endl << "BFS Result:" << endl << endl;
+
+    for (string i: BFSworkingMap.mapMatrix) {
+        cout << i << endl;
+    }
+
+    cout << endl << "Benchmark:" << endl << "DFS Path length is " << DFSbenchmark.pathLength(DFSworkingMap) << " and took " << DFStime << " ms to run." << endl
+        << "BFS Path length is " << BFSbenchmark.pathLength(BFSworkingMap) << " and took " << BFStime << " ms to run." << endl;
 
 
     cout << endl;
