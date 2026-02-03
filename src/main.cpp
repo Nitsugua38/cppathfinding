9682ccee3eeb4ffd8ec44182c1671c37302676b7
AugustinS <58813858+Nitsugua38@users.noreply.github.com>
2026-02-03 11:50:22 +0100

    feat: added benchmark and restructured code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <chrono>
 
 using namespace std;
 
@@ -25,6 +26,21 @@ class ALGOS {
         void findStart(MAP& m);
         bool dfs(MAP& m, int CurrentX, int CurrentY);
 };
+
+class BENCHMARK {
+    private:
+        chrono::time_point<chrono::high_resolution_clock> start, end;
+        chrono::duration<float> duration;
+
+    public:
+        int pathLength(MAP m);
+        void startTimer();
+        float stopTimer();
+};
+
+
+
+
 
 
 
@@ -58,6 +74,12 @@ void MAP::initMap(string mapPath) {
 
 
 
+
+
+
+
+
+
 // ---------------- FIND MAP STARTING POINT -------------------------
 
 void ALGOS::findStart(MAP& m) {
@@ -81,26 +103,77 @@ void ALGOS::findStart(MAP& m) {
 
 bool ALGOS::dfs(MAP& m, int CurrentX, int CurrentY) {
 
-    if (m.mapMatrix[CurrentY][CurrentX] == '#' || m.mapMatrix[CurrentY][CurrentX] == '*' || m.mapMatrix[CurrentY][CurrentX] == 'v') {
+    // Step 1 : if blocked/visited -> abandon
+
+    if (m.mapMatrix[CurrentY][CurrentX] == '#' || m.mapMatrix[CurrentY][CurrentX] == '*' || m.mapMatrix[CurrentY][CurrentX] == 'v' || m.mapMatrix[CurrentY][CurrentX] == '.') {
         return false;
     };
+
+
+    // Step 2 : if arrived -> finish
 
     if (m.mapMatrix[CurrentY][CurrentX] == 'E') {
         return true;
     };
 
 
-    m.mapMatrix[CurrentY][CurrentX] = 'v';
+    // Step 3 : mark as visited
+
+    if (m.mapMatrix[CurrentY][CurrentX] != 'S') m.mapMatrix[CurrentY][CurrentX] = 'v';
 
 
-    if (dfs(m, CurrentX + 1, CurrentY)) return true;
-    if (dfs(m, CurrentX - 1, CurrentY)) return true;
-    if (dfs(m, CurrentX, CurrentY + 1)) return true;
-    if (dfs(m, CurrentX, CurrentY - 1)) return true;
+    // Step 4 : explore neighbors
+
+    if (dfs(m, CurrentX + 1, CurrentY) || dfs(m, CurrentX - 1, CurrentY) || dfs(m, CurrentX, CurrentY + 1) || dfs(m, CurrentX, CurrentY - 1)) {
+        if (m.mapMatrix[CurrentY][CurrentX] != 'S') m.mapMatrix[CurrentY][CurrentX] = '.';
+        return true;
+    }
 
     return false;
 
 };
+
+
+
+
+
+
+
+
+
+
+
+// ---------------- BENCHMARK -------------------------
+
+int BENCHMARK::pathLength(MAP m) {
+    
+    int pl = 0;
+
+    for (int x = 0; x < m.NUM_COLUMNS; x++) {
+        for (int y = 0; y < m.NUM_ROWS; y++) {
+            if (m.mapMatrix[y][x] == '.') {
+                pl++;
+            };
+        }
+    }
+
+    return pl;
+
+};
+
+
+void BENCHMARK::startTimer() {
+    start = chrono::high_resolution_clock::now();
+};
+
+
+float BENCHMARK::stopTimer() {
+    end = chrono::high_resolution_clock::now();
+    duration = end - start;
+    float ms = duration.count() * 1000.0f;
+    return ms;
+}
+
 
 
 
@@ -116,31 +189,36 @@ bool ALGOS::dfs(MAP& m, int CurrentX, int CurrentY) {
 int main() {
 
     MAP workingMap;
-    workingMap.initMap("../maps/map1.txt");
-
-
-
-    cout << workingMap.NUM_COLUMNS << endl << workingMap.NUM_ROWS << endl << workingMap.NUM_VERTICES << endl;
-
-    for (string i: workingMap.mapMatrix) {
-        cout << i << endl;
-    }
-
-
     ALGOS algos;
+
+    
+    workingMap.initMap("../maps/map11.txt");
+    cout << "Imported map has " << workingMap.NUM_COLUMNS << " columns, " << workingMap.NUM_ROWS << " rows and " << workingMap.NUM_VERTICES << " vertices." << endl;
+
+
+    
     algos.findStart(workingMap);
-
-    cout << endl << "Starting point is located at " << workingMap.StartX << "," << workingMap.StartY << endl << endl;
-
+    cout << "Starting point is located at " << workingMap.StartX << "," << workingMap.StartY << endl << endl << "Result:" << endl << endl;
 
 
 
-    algos.dfs(workingMap, workingMap.StartX, workingMap.StartY);
 
-    for (string i: workingMap.mapMatrix) {
+    MAP DFSworkingMap = workingMap;
+    BENCHMARK DFSbenchmark;
+
+    DFSbenchmark.startTimer();
+    algos.dfs(DFSworkingMap, DFSworkingMap.StartX, DFSworkingMap.StartY);
+    float DFStime = DFSbenchmark.stopTimer();
+    
+    for (string i: DFSworkingMap.mapMatrix) {
         cout << i << endl;
     }
 
 
+
+    cout << endl << "Benchmark:" << endl << "DFS Path length is " << DFSbenchmark.pathLength(DFSworkingMap) << " and took " << DFStime << " ms to run." << endl;
+
+
+    cout << endl;
     return 0;
 }
